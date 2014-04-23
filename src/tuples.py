a7b98acd28fc942f85cc609634c7caa08d81f8f5
jaythaceo <jaythaceo@gmail.com>
2014-04-23 18:27:44 -0400

    Added dictionary

---
+++ src/tuples.py
@@ -0,0 +1,15 @@
+#!/usr/bin
+
+"""
+Tuples are similar to list
+except they are immutable.
+Once you setup a tuple
+there is now way to change .
+Other wise tuples work
+identically to lists.
+"""
+tup = "rocks", 0, "the universe"
+print(tup)
+
+index = tup[0:2]
+print(index)
