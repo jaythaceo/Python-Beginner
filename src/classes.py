878e1cedbc0b0fad2ea3b71ec0abe5b0729558db
jaythaceo <jaythaceo@gmail.com>
2014-06-09 16:33:44 -0400

    modified classes

--- src/classes.py
+++ src/classes.py
@@ -5,25 +5,23 @@ class Bird:
     def __init__(self, kind, call):
         self.call = call
         self.kind = kind
-        
+
     def do_call(self):
         print "a %s goes %s" % (self.kind, self.call)
-        
-        
+
 class Parrot(Bird):
     def __init__(self):
         Bird.__init__(self,"Parrot", "KahKah!")
-        
 
 class Cukoo(Bird):
     def __init__(self):
         Bird.__init__(self, "Cuckoo", "Cuckoo!")
-        
+
 
 if __name__ == "__main__":
     parrot = Parrot()
     cuckoo = Cukoo()
-    
+
     parrot.do_call()
     cuckoo.do_call()
 
