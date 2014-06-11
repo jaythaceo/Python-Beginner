6c1566e654d54cd6b7de69b0b8f943be6893788f
jaythaceo <jaythaceo@gmail.com>
2014-06-11 00:42:29 -0400

    Fixed classes.py

--- src/classes.py
+++ src/classes.py
@@ -9,9 +9,11 @@ class Bird:
     def do_call(self):
         print "a %s goes %s" % (self.kind, self.call)
 
+
 class Parrot(Bird):
     def __init__(self):
         Bird.__init__(self,"Parrot", "KahKah!")
+
 
 class Cukoo(Bird):
     def __init__(self):
@@ -21,8 +23,6 @@ class Cukoo(Bird):
 if __name__ == "__main__":
     parrot = Parrot()
     cuckoo = Cukoo()
-
+    
     parrot.do_call()
     cuckoo.do_call()
-
-
