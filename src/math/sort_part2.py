c4d25e1e20361070eb40be5744421f7dae481c01
jaythaceo <jaythaceo@gmail.com>
2014-11-08 23:03:25 -0500

    added files

---
+++ src/math/sort_part2.py
@@ -0,0 +1,6 @@
+"""
+Another sorting algorithm
+
+"""
+
+
--- src/strings/challenge.py
+++ src/strings/challenge.py
@@ -2,14 +2,17 @@
 import string
 import random
 
+length = 10
+
 def alphaString(length):
   return ''.join(
     random.choice(string.lowercase) for i in range(length)
     )
 
 def random_numberics(length):
+  i  = length
   for i in range(length):
-    return random.sample(string.letters + string.digits, length)
+    return random.choice(string.letters + string.digits)
 
 def regNumber(length, x):
   return random.uniform(length, x)
@@ -18,15 +21,8 @@ def realNumber(length):
   return ''.join(
     random.choice(string.digits) for i in range(length))
 
-def randStr(length):
 
-
-
-"""
+print alphaString(10)
 print regNumber(10, 100)
 print alphaString(10)
-print realNumber(10)
 print random_numberics(10)
-"""
-
-
