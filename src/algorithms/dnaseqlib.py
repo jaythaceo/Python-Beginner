a9064414d6c0b60a0cd5e12eec0b8e5592496943
jaythaceo <jaythaceo@gmail.com>
2015-09-22 12:45:48 -0400

    [chore] Completed driver code

--- src/algorithms/dnaseqlib.py
+++ src/algorithms/dnaseqlib.py
@@ -36,4 +36,28 @@ class Array2D:
 
 ### High-level driver code. No need to tweak this EVER. ###
 
-
+# Given a sequence of matches, produces a w-by-h image and saves it to filename.
+# The remapping function takes values in (0,1) and returns values in (0,1); the default
+# value (fourth-root) makes lightly-populated bins considerably darker.
+def buildComparisonImage(filename, w, h, alen, blen, matches, remapfn=lambda x:math.sqrt(math.sqrt(x))):
+    """docstring for buildComparisonImage"""
+    arr = Array2D('L', w, h, 0L)
+    print "Sequence A length: " + str(alen)
+    print "Sequence B length: " + str(blen)
+    abinsize = int(math.ceil(alen / float(w)))
+    bbinsize = int(math.ceil(blen / float(h)))
+    assert abinsize > 0 and bbinsize > 0
+    print "Binnning matches..."
+    for m in matches:
+        #print m (abinsize, bbinsize), (m[0]//abinsize, m[1]//bbinsize), (w, h)
+        arr.incr(m[0] // abinsize, m[1] // bbinsize)
+        print "...done binning matches."
+        print "Normalizing and plotting results..."
+        maxval = float(arr.max())
+        img = Image.new('RGB', (w,h))
+        for y in range(0, h):
+            for x in range(0, w):
+                val = 255 - int(math.ceil(remapfn((arr.get(x,y) / maxval)) * 255.0))
+                img.putpixel((x,y), (val, val, val))
+        print "...done normalizing and plotting."
+        img.save(filename)
