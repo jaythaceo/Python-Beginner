aaa4d708f6eb3d58c322ae6560ebd30574612018
jaythaceo <jaythaceo@gmail.com>
2016-03-28 14:26:45 -0400

    [chore] Added new files

--- src/textFiles/parse.py
+++ src/textFiles/parse.py
@@ -1,7 +1,8 @@
 #!/usr/bin/
+fileName = raw_input("Enter the file name: ")
+try:
+	fileName = open(fileName)
+except:
+	print "The file cannot be opened: ", fileName
+	exit()
 
-f = open('text.txt', 'r')
-for line in f:
-    if line.startswith('Jason'):
-        print line
-f.close()
--- src/textFiles/words.py
+++
@@ -1,8 +0,0 @@
-"""
-Parse and sort
-csv log files
-"""
-a = [12,45,54,3,2,3]
-
-for x in a:
-	print a
