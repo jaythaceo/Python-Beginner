2dc369ded9f15058d6dc74533c1b6c234a6f49dc
jaythaceo <jaythaceo@gmail.com>
2016-03-28 22:50:39 -0400

    [chore] Added line count

--- src/textFiles/parse.py
+++ src/textFiles/parse.py
@@ -4,5 +4,12 @@ try:
 	fileName = open(fileName)
 except:
 	print "The file cannot be opened: ", fileName
-	exit()
 
+count = 0 
+for line in fileName:
+	count = count + 1
+print "Lines is file: ", count
+
+
+
+exit() 
