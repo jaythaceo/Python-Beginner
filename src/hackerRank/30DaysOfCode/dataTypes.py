a5024181fea86d8a0616d9e0db8905225bfe394f
jaythaceo <jaythaceo@gmail.com>
2016-05-20 06:36:09 -0400

    [chore] Added dataTypes day 1

---
+++ src/hackerRank/30DaysOfCode/dataTypes.py
@@ -0,0 +1,16 @@
+
+
+i = 4
+d = 4.0
+s = "HackerRank"
+
+second_int = input()
+second_d = input()
+new_s = raw_input()
+
+second_int = second_int + i
+second_d = second_d + d
+new_s = s + ' ' + new_s
+print(second_int)
+print(second_d)
+print(new_s)
