3548893aae91b7af1ceaa8de58c11bd1fb3ffabf
jaythaceo <jaythaceo@gmail.com>
2016-07-01 15:07:06 -0400

    [chroe] Added odd_even

---
+++ src/hackerRank/odd_even.py
@@ -0,0 +1,5 @@
+import sys
+
+
+N = int(raw_input().strip())
+print N
--- src/textFiles/box.py
+++ src/textFiles/box.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python
 
+import csv
+
+f = open('baltimore.csv')
+csv_f = csv.reader(f)
+
+for row in csv_f:
+	print row[12:4]
