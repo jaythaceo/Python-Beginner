c543acedfcf7444a49c3cec4d6ae6782f70e1140
jaythaceo <jaythaceo@gmail.com>
2016-07-11 01:12:32 -0400

    [chore] Added linkedList class & stack methods

---
+++ src/algorithms/linkedList.py
@@ -0,0 +1,21 @@
+class LinkedStack:
+	""" LIFO Stack inplementation using a singly linked list for storage"""
+
+	#---------------- nested _Node Class --------------------------
+	class _Node():
+		"""Lightweight, nonpublic class for sorting s singly linked node"""
+		__slots__ = '_element', '_next'
+
+		def __init__(self, element, next):
+			self.element = element
+			self.next = next
+			
+	#------------------ Stack methods ----------------------------
+	def __init__(self):
+		"""Create an empty stack"""
+		self._head = None
+		self._size = 0
+
+	def __len__(self):
+		"""Return the number of elements in the stack"""
+		return self._size
