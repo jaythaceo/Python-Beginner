30a67551e229b1931347eed93b5121a4a13247ae
jaythaceo <jaythaceo@gmail.com>
2016-09-15 08:51:41 -0400

    [chore] Added weather script.

---
+++ src/basics/simple_program.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+import urllib2
+import json
+
+# API key
+key = "8fbbbbde07d55f78"
+
+while 1:
+	zip = raw_input("Enter your zipcode ")
+	url = 'http://api.wunderground.com/api/' + key + '/geolookup/conditions/q/PA/' + zip + '.json'
+	f = urllib2.urlopen(url)
+	json_string = f.read()
+	parsed_json = json.loads(json_string)
+	city = parsed_json['location']['city']
+	state = parsed_json['location']['state']
+	weather = parsed_json['current_observation']['weather']
+	temperature_string = parsed_json['current_observation']['temperature_string']
+	feelslike_string = parsed_json['current_observation']['feelslike_string']
+	print 'Weather in ' + city + ', ' + state + ': ' + weather.lower() + '. The temperature is ' + temperature_string + ' but it feels like ' + feelslike_string + '.'
+	f.close()
